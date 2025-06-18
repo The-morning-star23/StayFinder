ff53942989e2e7a6a8b732805cb2744a0e10c093
The-morning-star23 <kumarshubh263@gmail.com>
2025-06-18 20:35:04 +0530

    solved chunk problem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('mapbox-gl')) return 'mapbox';
+            return 'vendor';
+          }
+        },
+      },
+      chunkSizeWarningLimit: 1000, // Optional: Increase limit to suppress warning
+    },
+  },
+});
