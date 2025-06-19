d7508322600b04937de397e0100693409608a1d6
The-morning-star23 <kumarshubh263@gmail.com>
2025-06-19 13:14:01 +0530

    updated url in axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://stayfinder-backend-4qgs.onrender.com',
+  baseURL: "https://stayfinder-backend-4qgs.onrender.com/api",
 });
 
 export default instance;
